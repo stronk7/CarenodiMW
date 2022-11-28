6f4337830815be53132008975dc78b0ebd0f1e11
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2022-11-28 17:55:12 +0100

    Here we go, first cut of the extension

---
+++ README.md
@@ -0,0 +1,51 @@
+# CarenodiMW
+
+CarenodiMW is a mediawiki extension that adds the [rel=canonical](https://en.wikipedia.org/wiki/Canonical_link_element) and the [meta=refresh](https://en.wikipedia.org/wiki/Meta_refresh) tags to pages which wikitext matches a regexp-based configuration.
+
+Note that this extension manages both features as HTML tags, not as HTTP headers.
+
+## Installation
+
+- Current version requires `MediaWiki >=1.31 <1.36`
+- [Download and unzip it](https://github.com/stronk7/CarenodiMW/releases) (or clone this git repo).
+- Copy the folder as `CarenodiMW` within the `extensions`directory of your mediawiki site.
+
+## Usage
+
+### Enable the extension
+
+Edit `LocalSettings.php` and add the following, configuring the settings to suit your needs.
+
+```php
+wfLoadExtension('CarenodiMW'); // Load me!
+
+// Base URL to be used for all the canonical and refresh tags.
+$wgCarenodiMW_base = 'https://example.com';
+
+// Regular expression to extract the path for the canonical tags.
+// The first captured group in the regular expression will be used.
+// Any empty value disables the feature globally.
+$wgCarenodiMW_canonical_regexp = '/new location is (.*) this/';
+
+// Regular expression to extract the path for the refresh tags.
+// The first captured group in the regular expression will be used)
+// Any empty value disables the feature globally.
+// (note that, normally, it will be the same than the canonical one
+// but it's possible to specify different one).
+$wgCarenodiMW_refresh_regexp = '/new location is (.*) this/';
+
+// Number of seconds to perform the refresh to new URL (0 = immediate).
+$wgCarenodiMW_refresh_wait = 5;
+
+// Enable separate debug for the extension
+// $wgDebugLogGroups['CarenodiMW'] = '/tmp/CarenodiMW.log';
+
+```
+
+## Contributing
+
+[Pull requests](https://github.com/stronk7/CarenodiMW/pulls) are welcome. For major changes, please [open an issue](https://github.com/stronk7/CarenodiMW/issues) first to discuss what you would like to change.
+
+## License and copyright
+
+[BSD 3-Clause](https://choosealicense.com/licenses/bsd-3-clause/) - Copyright (c) 2022 onwards, Eloy Lafuente (stronk7).
---
+++ includes/CarenodiMW.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace MediaWiki\Extension;
+
+use ConfigFactory;
+use ContentHandler;
+use MediaWiki\Revision\RevisionRecord;
+use OutputPage;
+use Skin;
+use WikiPage;
+
+/**
+ * Main CarenodiMW class implementation.
+ *
+ * @copyright 2022 onwards Eloy Lafuente (stronk7)
+ * @license https://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ */
+class CarenodiMW {
+
+    /**
+     * Add the rel=canonical and the meta=refresh tags to pages matching a regexp-based configuration.
+     *
+     * @param OutputPage &$out
+     * @param Skin &$skin
+     */
+    public static function onBeforePageDisplay(OutputPage &$out, Skin &$skin) {
+
+        // We are going to apply the changes to existing article pages.
+        if (!$out->getTitle()->exists()) { // Non-existing page, nothing to do.
+            return true;
+        }
+        if ($out->getTitle()->isTalkPage()) { // Talk pages, nothing to do.
+            return true;
+        }
+        if (!$out->isArticle()) { // Non-article page, nothing to do.
+            return true;
+        }
+        if ($out->getPageTitle() !== $out->getTitle()->getText()) { // Page and title don't match, nothing to do.
+            return true;
+        }
+
+        wfDebugLog('CarenodiMW', __METHOD__ . ' called for "' . $out->getPageTitle() . '"');
+
+        // Arrived here, let's verify the configuration.
+        $config = ConfigFactory::getDefaultInstance()->makeConfig('CarenodiMW');
+        $base = $config->get('CarenodiMW_base');
+        $canonicalRegexp = $config->get('CarenodiMW_canonical_regexp');
+        $refreshRegexp = $config->get('CarenodiMW_refresh_regexp');
+        $refreshWait = $config->get('CarenodiMW_refresh_wait');
+
+        if (empty($base) || (empty($canonicalRegexp) && empty($refreshRegexp))) {
+            wfDebugLog('CarenodiMW', __METHOD__ . ' - Missing configuration. Nothing to do');
+            return true;
+        }
+
+        if (@preg_match($canonicalRegexp, '') === false) {
+            wfDebugLog('CarenodiMW', __METHOD__ . ' - Invalid $wgCarenodiMW_canonical_regexp regular expression found');
+            return true;
+        }
+
+        if (@preg_match($refreshRegexp, '') === false) {
+            wfDebugLog('CarenodiMW', __METHOD__ . ' - Invalid $wgCarenodiMW_refresh_regexp regular expression found');
+            return true;
+        }
+
+        if (!is_int($refreshWait) || $refreshWait < 0) {
+            wfDebugLog('CarenodiMW', __METHOD__ . ' - Invalid $wgCarenodiMW_refresh_wait positive integer found');
+            return true;
+        }
+
+        // This is not ideal, but after playing with different hooks, individually or combining them, it's
+        // the easiest way to access to the original wikitext of the page that we want to examine against
+        // our regular expressions. 4-5 queries are added to the page.
+        if (!$wikiPage = WikiPage::factory($out->getTitle())) {
+            wfDebugLog('CarenodiMW', __METHOD__ . ' - Something went wrong, cannot get the original wikitext');
+        }
+        $wikitext = ContentHandler::getContentText($wikiPage->getContent(RevisionRecord::RAW));
+
+        // Do we need to add the rel=canonical.
+        $path = '';
+        if ($canonicalRegexp) {
+            if (preg_match($canonicalRegexp, $wikitext, $matches) && isset($matches[1])) {
+                $path = $matches[1];
+            }
+            // Let's add the link=canonical tag if a path for it has been found.
+            if ($path) {
+                $canonical = trim($base, ' /\\') . '/' . trim($path, ' /\\');
+                $out->setCanonicalUrl($canonical);
+                wfDebugLog('CarenodiMW', __METHOD__ . ' - Adding rel=canonical: ' . $canonical);
+            } else {
+                wfDebugLog('CarenodiMW', __METHOD__ . ' - $wgCarenodiMW_canonical_regexp not matched');
+            }
+        }
+
+        // Let's grep to add the meta=refresh.
+        if ($refreshRegexp) {
+            // Shortcut if both regular expressions are the same to save one execution.
+            if ($refreshRegexp !== $canonicalRegexp) {
+                $path = '';
+                if (preg_match($refreshRegexp, $wikitext, $matches) && isset($matches[1])) {
+                    $path = $matches[1];
+                }
+            }
+            // Let's add the meta=refresh tag if a path for it has been found.
+            if ($path) {
+                $refresh = trim($refreshWait ) . '; ' . trim($base, ' /\\') . '/' . trim($path, ' /\\');
+                $out->addMeta('http:refresh', $refresh);
+                wfDebugLog('CarenodiMW', __METHOD__ . ' - Adding meta=refresh: ' . $refresh);
+            } else {
+                wfDebugLog('CarenodiMW', __METHOD__ . ' - $wgCarenodiMW_refresh_regexp not matched');
+            }
+        }
+    }
+}
